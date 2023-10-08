e74f4297945f5af2b382ec4dcd1347b7d0ee495d
Tadek Waluga <tadek.waluga@gmail.com>
2023-10-08 20:53:25 +0200

    Added Field component

---
+++ src/common/Field/index.js
@@ -0,0 +1,11 @@
+import { Button } from "./styled";
+
+const Field = () => {
+  return (
+    <>
+      <Button />
+    </>
+  )
+};
+
+export default Field;
---
+++ src/common/Field/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  height: 50px;
+  width: 50px;
+  border: 1px solid black;
+  background-color: blue;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightblue;
+  }
+`;
